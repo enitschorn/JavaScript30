ff5104c374dbc7b2b1f3188af89fc08ed532e18c
Christine Schorn <christine.schorn@ie.com.au>
2020-09-22 11:30:31 +1000

    DAY 11 add fullscreen option

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
+const fullscreen = player.querySelector(".fullscreen");
 
 // Build our functions
 const togglePlay = () => {
@@ -41,6 +42,10 @@ const scrub = (e) => {
   video.currentTime = scrubTime;
 };
 
+const toggleFullscreen = () => {
+  video.requestFullscreen();
+};
+
 // Hook up the event listener
 toggle.addEventListener("click", togglePlay);
 
@@ -61,3 +66,5 @@ progress.addEventListener("click", scrub);
 progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
 progress.addEventListener("mousedown", () => (mousedown = true));
 progress.addEventListener("mouseup", () => (mousedown = false));
+
+fullscreen.addEventListener("click", toggleFullscreen);
